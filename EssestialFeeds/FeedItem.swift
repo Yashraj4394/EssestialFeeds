fd5310886686815eb80305824c848572c575b313
YashraJ <yashraj.gujar@icloud.com>
2022-03-19 17:04:53 +0530

    Created FeedItemModel

---
+++ EssestialFeeds/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssestialFeeds
+//
+//  Created by YashraJ Gujar on 19/03/22.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
