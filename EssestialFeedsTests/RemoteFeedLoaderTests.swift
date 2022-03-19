0ddabe33c7e69ff37c86e9007334e81b58ee3da2
YashraJ <yashraj.gujar@icloud.com>
2022-03-19 23:32:28 +0530

    refactor test to remove duplication

--- EssestialFeedsTests/RemoteFeedLoaderTests.swift
+++ EssestialFeedsTests/RemoteFeedLoaderTests.swift
@@ -7,23 +7,6 @@
 
 import XCTest
 
-class RemoteFeedLoaderTests: XCTestCase {
-	
-	func test_init_doesNotRequestDatFromURL(){ // asset that url should be nil unless load function is called from RemoteFeedLoader
-		let client = HTTPClientSpy()
-		let _ = RemoteFeedLoader(url: URL(string: "https://www.a-url.com")!, client: client)
-		XCTAssertNil(client.requestedURL)
-	}
-	
-	func test_load_requestDataFromURL(){
-		let url = URL(string: "https://www.a-new-url.com")!
-		let client = HTTPClientSpy()
-		let sut = RemoteFeedLoader(url: url, client: client)
-		sut.load()
-		XCTAssertEqual(client.requestedURL,url)
-	}
-}
-
 class RemoteFeedLoader {
 	let client : HTTPClient
 	let url: URL
@@ -44,10 +27,35 @@ protocol HTTPClient {
 	func getFrom(from url: URL)
 }
 
-// created a new subclass so that it can be tested as HTTPClient will be part of production code and we dont want requeted url to be a part of it. Test logic is now in the spy
-class HTTPClientSpy: HTTPClient {
-	func getFrom(from url: URL) {
-		requestedURL = url
+class RemoteFeedLoaderTests: XCTestCase {
+	
+	func test_init_doesNotRequestDatFromURL(){ // asset that url should be nil unless load function is called from RemoteFeedLoader
+		let (_, client) = makeSUT()
+		XCTAssertNil(client.requestedURL)
 	}
-	var requestedURL: URL?
+	
+	func test_load_requestDataFromURL(){
+		let url = URL(string: "https://www.a-new-url.com")!
+		let (sut,client) = makeSUT(url: url)
+		sut.load()
+		XCTAssertEqual(client.requestedURL,url)
+	}
+	
+	
+	//MARK: HELPERS
+	private func makeSUT(url: URL =  URL(string: "https://www.a-url.com")!) -> (sut:RemoteFeedLoader,client: HTTPClientSpy){
+		let client = HTTPClientSpy()
+		let sut = RemoteFeedLoader(url: url, client: client)
+		return (sut,client)
+	}
+	
+	private class HTTPClientSpy: HTTPClient {
+		
+		var requestedURL: URL?
+		
+		func getFrom(from url: URL) {
+			requestedURL = url
+		}
+	}
+	
 }
