85298c47b3a2aa74fee039d64c559a534eff2e45
YashraJ <yashraj.gujar@icloud.com>
2022-03-19 22:31:58 +0530

    request data from url on 'load'
    Created HTTPClient as singleton

--- EssestialFeedsTests/RemoteFeedLoaderTests.swift
+++ EssestialFeedsTests/RemoteFeedLoaderTests.swift
@@ -10,18 +10,28 @@ import XCTest
 class RemoteFeedLoaderTests: XCTestCase {
 	
 	func test_init_doesNotRequestDatFromURL(){ // asset that url should be nil unless load function is called from RemoteFeedLoader
-		let client = HTTPClient()
+		let client = HTTPClient.shared
 		let _ = RemoteFeedLoader()
 		XCTAssertNil(client.requestedURL)
+	}
+	
+	func test_load_requestDataFromURL(){
+		let client = HTTPClient.shared
+		let sut = RemoteFeedLoader()
+		sut.load()
+		XCTAssertNotNil(client.requestedURL)
+	}
+}
+
+class RemoteFeedLoader {
+	func load(){
+		HTTPClient.shared.requestedURL = URL(string: "https://www.a-url.com")
 	}
 }
 
 //http client that is going to connect with the network
-
-class RemoteFeedLoader {
-	
-}
-
 class HTTPClient {
+	static let shared = HTTPClient()
+	private init(){}
 	var requestedURL: URL?
 }
