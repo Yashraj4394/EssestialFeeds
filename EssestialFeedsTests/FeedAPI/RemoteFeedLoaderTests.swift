0b153d9504443281f0fc9f60ad31c8fc87aac49f
YashraJ <yashraj.gujar@icloud.com>
2022-03-22 18:45:15 +0530

    remove the generic error type from 'LoadFeedResult' to simplify type constraints

--- EssestialFeedsTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssestialFeedsTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -33,7 +33,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 	func test_load_deliversErrorOnClientError(){
 		let (sut,client) = makeSUT()
 		
-		expect(sut, toCompleteWith: .failure(.connectivity), when: {
+		expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
 			
 			let clientError = NSError(domain: "test", code: 0)
 			client.complete(with: clientError)
@@ -44,7 +44,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 		let (sut,client) = makeSUT()
 		let samples = [199,201,300,400,400]
 		samples.enumerated().forEach { (index,code) in
-			expect(sut, toCompleteWith: .failure(.invalidData), when: {
+			expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
 				let json = makeItemJSON([])
 				client.complete(withStatusCode: code,data: json ,at: index)
 			})
@@ -54,7 +54,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 	//success with 200 but invalid json
 	func test_load_deliversErrorOn200HTTPResponseWithInvalidData(){
 		let (sut,client) = makeSUT()
-		expect(sut, toCompleteWith: .failure(.invalidData), when: {
+		expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
 			let invalidJson = Data("invalid json".utf8)
 			client.complete(withStatusCode: 200, data: invalidJson)
 		})
@@ -144,7 +144,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 			switch (receivedResult,expectedResult) {
 				case let (.success(receivedItems),.success(expedtedItems)):
 					XCTAssertEqual(receivedItems, expedtedItems,file: file,line: line)
-				case let (.failure(receivedError),.failure(expectedError)):
+				case let (.failure(receivedError as RemoteFeedLoader.Error),.failure(expectedError as RemoteFeedLoader.Error)):
 					XCTAssertEqual(receivedError, expectedError,file: file,line: line)
 				default:
 					XCTFail("expected result \(expectedResult) got \(receivedResult) instead",file: file,line: line)
