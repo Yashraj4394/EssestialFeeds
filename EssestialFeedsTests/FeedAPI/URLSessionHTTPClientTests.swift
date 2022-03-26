2a70a74ebc631bb9ba9f929937bcc22b16f23f38
YashraJ <yashraj.gujar@icloud.com>
2022-03-26 20:49:33 +0530

    MemoryLeak Helper
    extract memory leak tracking helper method to a shared extension on XCTestCase

--- EssestialFeedsTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssestialFeedsTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -87,12 +87,6 @@ class URLSessionHTTPClientTests: XCTestCase {
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return sut
 	}
-	
-	private func trackForMemoryLeaks(_ instance: AnyObject,file: StaticString = #filePath, line: UInt = #line) {
-		addTeardownBlock { [weak instance] in
-			XCTAssertNil(instance,"Instance should have been deallocated. Potential memory leak",file: file,line: line)
-		}
-	}
 	//class URLProtocol : NSObject : An abstract class that handles the loading of protocol-specific URL data.
 	private class URLProtocolStub: URLProtocol {
 		private static var stub : Stub?
