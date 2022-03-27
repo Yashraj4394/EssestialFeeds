28515cb7a4791558cd0cd377a0d677037353bc94
YashraJ <yashraj.gujar@icloud.com>
2022-03-27 11:50:50 +0530

    Update URLSessionHTTPClientTests.swift
    Extract duplicate code into a new shared reusable helper method to simplify tests

--- EssestialFeedsTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssestialFeedsTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -84,41 +84,22 @@ class URLSessionHTTPClientTests: XCTestCase {
 	func test_getFromURL_succeedsOnHTTPURLResponseWithValidData(){
 		let data = anyData()
 		let response = anyHTTPURLResponse()
-		URLProtocolStub.stub(data: data, response: response, error: nil)
-		let exp = expectation(description: "wait for completion")
-		makeSUT().get(from: anyURL()) { result in
-			switch result {
-				case let .success(receivedData, receivedResponse):
-					XCTAssertEqual(receivedData, data)
-					XCTAssertEqual(receivedResponse.url, response.url)
-					XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
-				default:
-					XCTFail("expected success , got \(result) instead")
-			}
-			exp.fulfill()
-		}
 		
-		wait(for: [exp], timeout: 1.0)
+		let receivedValues = resultValuesFor(data: data, response: response, error: nil)
+		
+		XCTAssertEqual(receivedValues?.data,data)
+		XCTAssertEqual(receivedValues?.response.url, response.url)
+		XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
 	}
 	
 	func test_getFromURL_succeedsWithEmotyDataOnHTTPURLResponseWithNilData(){
 		let response = anyHTTPURLResponse()
-		URLProtocolStub.stub(data: nil, response: response, error: nil)
-		let exp = expectation(description: "wait for completion")
-		makeSUT().get(from: anyURL()) { result in
-			switch result {
-				case let .success(receivedData, receivedResponse):
-					let emptyData = Data()
-					XCTAssertEqual(receivedData,emptyData)
-					XCTAssertEqual(receivedResponse.url, response.url)
-					XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
-				default:
-					XCTFail("expected success , got \(result) instead")
-			}
-			exp.fulfill()
-		}
+		let receivedValues = resultValuesFor(data: nil, response: response, error: nil)
 		
-		wait(for: [exp], timeout: 1.0)
+		let emptyData = Data()
+		XCTAssertEqual(receivedValues?.data,emptyData)
+		XCTAssertEqual(receivedValues?.response.url, response.url)
+		XCTAssertEqual(receivedValues?.response.statusCode, response.statusCode)
 	}
 	
 	//MARK: - HELPERS
@@ -150,26 +131,44 @@ class URLSessionHTTPClientTests: XCTestCase {
 	}
 	
 	private func resultErrorFor(data: Data?,response: URLResponse?,error: Error?,file: StaticString = #filePath, line: UInt = #line) -> Error? {
+		
+		let result = resultFor(data: data, response: response, error: error,file: file,line: line)
+		switch result {
+			case let .failure(error):
+				return error
+			default:
+				XCTFail("Expected failure , but got \(result) instead",file: file,line: line)
+				return nil
+		}
+	}
+	
+	private func resultValuesFor(data: Data?,response: URLResponse?,error: Error?,file: StaticString = #filePath, line: UInt = #line) -> (data: Data?,response: HTTPURLResponse)? {
+		let result = resultFor(data: data, response: response, error: error,file: file,line: line)
+		switch result {
+			case let .success(data, response):
+				return (data,response)
+			default:
+				XCTFail("Expected success , but got \(result) instead",file: file,line: line)
+				return nil
+		}
+	}
+	
+	private func resultFor(data: Data?,response: URLResponse?,error: Error?,file: StaticString = #filePath, line: UInt = #line) -> HTTPClientResult {
 		URLProtocolStub.stub(data: data, response: response, error: error)
 		let sut = makeSUT(file:file,line: line)
 		
 		//as getMethod is async, we have add expectation
 		let exp = expectation(description: "wait for completion")
 		
-		var receivedError: Error?
+		var receivedResult: HTTPClientResult!
 		
 		sut.get(from : anyURL()) { result in
-			switch result {
-				case let .failure(error):
-					receivedError = error
-				default:
-					XCTFail("Expected failure , but got \(result) instead",file: file,line: line)
-			}
+			receivedResult = result
 			exp.fulfill()
 		}
 		
 		wait(for: [exp], timeout: 1.0)
-		return receivedError
+		return receivedResult
 	}
 	
 	//class URLProtocol : NSObject : An abstract class that handles the loading of protocol-specific URL data.
