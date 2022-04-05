d369b761574acc1ba2c93464bc3f68ad9b8c4477
YashraJ <yashraj.gujar@icloud.com>
2022-04-05 09:46:11 +0530

    Save command requests cache deletion

--- EssestialFeedsTests/CacheFeedUseCaseTests.swift
+++ EssestialFeedsTests/CacheFeedUseCaseTests.swift
@@ -6,17 +6,26 @@
 //
 
 import XCTest
+import EssestialFeeds
 
 class LocalFeedLoader {
-	//
+	private let store: FeedStore
 	init(store: FeedStore) {
-		
+		self.store = store
+	}
+	
+	func saveItems(_ items: [FeedItem]) {
+		store.deleteCacheFeed()
 	}
 }
 
 //the feedstore is a helper class representing a framework side to help us define the abstract interface the use case needs for its collaborator , making sure not to leak framework details into the use case.
 class FeedStore {
 	var deleteCachedFeedCallCount = 0
+	
+	func deleteCacheFeed(){
+		deleteCachedFeedCallCount += 1
+	}
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -27,5 +36,24 @@ class CacheFeedUseCaseTests: XCTestCase {
 		_ = LocalFeedLoader(store: store)// to decouple the applciation from framework details ,we dont let frameworks dictate the use case intrefaces (eg. adding Codable requirement or Core Data managed contexts parameters). We do so by test driving the interfaces the use case needs for its collaborators, rather than defining the interface upfront to facilitate a specific framework implementatioon.
 		XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
 	}
+	
+	func test_save_requestsCacheDeletion(){
+		let store = FeedStore()
+		let sut = LocalFeedLoader(store: store)
+		let items = [uniqueItems(),uniqueItems()]
+		sut.saveItems(items)
+		XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+	}
 
+	
+	//MARK: - HELPERS
+	func uniqueItems() -> FeedItem {
+		return FeedItem(id: UUID(), description: "anyDescription", location: "anyLocation", imageURL: anyURL())
+	}
+	
+	private func anyURL() -> URL {
+		let url = URL(string: "https://www.a-url.com")!
+		return url
+	}
+	
 }
