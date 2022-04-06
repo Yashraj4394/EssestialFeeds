f07681aa1401e8c55f083726e33abaa7433802c0
YashraJ <yashraj.gujar@icloud.com>
2022-04-06 10:21:18 +0530

    Add 'LocalFeedItem'
    Add 'LocalFeedItem' data transfer representation to decouple storage frameworks from 'FeedItem' data models

--- EssestialFeeds/FeedCache/FeedStore.swift
+++ EssestialFeeds/FeedCache/FeedStore.swift
@@ -12,6 +12,22 @@ public protocol FeedStore {
 	func deleteCacheFeed(completion: @escaping DeletionCompletion)
 	
 	typealias InsertionCompletion = (Error?) -> Void
-	func insert(_ items: [FeedItem],timestamp: Date,completion: @escaping InsertionCompletion)
+	func insert(_ items: [LocalFeedItem],timestamp: Date,completion: @escaping InsertionCompletion)
 	
+}
+
+public struct LocalFeedItem: Equatable {
+	//making constants public
+	public let id: UUID
+	public let description: String?
+	public let location: String?
+	public let imageURL: URL
+	
+	//struct has a default initialiazer but that is internal which is not accessible outside this module. So we create a public initializer
+	public init(id: UUID,description: String?,location: String?,imageURL: URL){
+		self.id = id
+		self.description = description
+		self.location = location
+		self.imageURL = imageURL
+	}
 }
--- EssestialFeedsTests/CacheFeedUseCaseTests.swift
+++ EssestialFeedsTests/CacheFeedUseCaseTests.swift
@@ -37,11 +37,13 @@ class CacheFeedUseCaseTests: XCTestCase {
 		let timestamp = Date()
 		let (sut,store) = makeSUT(currentDate: { timestamp })
 		let items = [uniqueItems(),uniqueItems()]
-		
+		let localItems = items.map {
+			LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL)
+		}
 		sut.saveItems(items) { _ in }
 		store.completeDeletionSuccessfully()
 		
-		XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed,.insert(items, timestamp)])
+		XCTAssertEqual(store.receivedMessages, [.deleteCachedFeed,.insert(localItems, timestamp)])
 	}
 	
 	func test_save_failsOnDeletionError(){
@@ -191,7 +193,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 	 
 	 enum ReceivedMessages: Equatable {
 		 case deleteCachedFeed
-		 case insert([FeedItem],Date)
+		 case insert([LocalFeedItem],Date)
 	 }
 	 
 	 private(set) var receivedMessages = [ReceivedMessages]()
@@ -209,7 +211,7 @@ class CacheFeedUseCaseTests: XCTestCase {
 		 deletionCompletions[index](nil)
 	 }
 	 
-	 func insert(_ items: [FeedItem],timestamp: Date,completion: @escaping InsertionCompletion) {
+	 func insert(_ items: [LocalFeedItem],timestamp: Date,completion: @escaping InsertionCompletion) {
 		 receivedMessages.append(.insert(items, timestamp))
 		 insertionCompletions.append(completion)
 	 }
