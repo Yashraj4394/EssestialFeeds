a54e16d0333fcf4b27e955b46197bd7c6c6fa5dc
YashraJ <yashraj.gujar@icloud.com>
2022-04-07 08:43:02 +0530

    Extract 'FeedStoreSpy' into a shared scope to remove duplication

--- EssestialFeedsTests/CacheFeedUseCaseTests.swift
+++ EssestialFeedsTests/CacheFeedUseCaseTests.swift
@@ -179,47 +179,6 @@ class CacheFeedUseCaseTests: XCTestCase {
 		XCTAssertEqual(receivedError as NSError?, expectedError,file:file,line: line)
 	}
 	
-	//the feedstore is a helper class representing a framework side to help us define the abstract interface the use case needs for its collaborator , making sure not to leak framework details into the use case.
-	class FeedStoreSpy: FeedStore {
-		
-		private var deletionCompletions = [DeletionCompletion]()
-		
-		private var insertionCompletions = [InsertionCompletion]()
-		
-		enum ReceivedMessages: Equatable {
-			case deleteCachedFeed
-			case insert([LocalFeedImage],Date)
-		}
-		
-		private(set) var receivedMessages = [ReceivedMessages]()
-		
-		func deleteCacheFeed(completion: @escaping DeletionCompletion){
-			deletionCompletions.append(completion)
-			receivedMessages.append(.deleteCachedFeed)
-		}
-		
-		func completeDeletion(with error:Error, at index: Int = 0) {
-			deletionCompletions[index](error)
-		}
-		
-		func completeDeletionSuccessfully(at index: Int = 0) {
-			deletionCompletions[index](nil)
-		}
-		
-		func insert(_ feed: [LocalFeedImage],timestamp: Date,completion: @escaping InsertionCompletion) {
-			receivedMessages.append(.insert(feed, timestamp))
-			insertionCompletions.append(completion)
-		}
-		
-		func completeInsertion(with error: Error,at index: Int = 0) {
-			insertionCompletions[index](error)
-		}
-		
-		func completeInsertionSuccessfuly(at index: Int = 0){
-			insertionCompletions[index](nil)
-		}
-	}
-	
 	func uniqueImage() -> FeedImage {
 		return FeedImage(id: UUID(), description: "anyDescription", location: "anyLocation", url: anyURL())
 	}
--- EssestialFeedsTests/LoadFeedFromCacheUseCaseTests.swift
+++ EssestialFeedsTests/LoadFeedFromCacheUseCaseTests.swift
@@ -26,46 +26,5 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
 		return (sut,store)
 	}
 	
-	//the feedstore is a helper class representing a framework side to help us define the abstract interface the use case needs for its collaborator , making sure not to leak framework details into the use case.
-	class FeedStoreSpy: FeedStore {
-		
-		private var deletionCompletions = [DeletionCompletion]()
-		
-		private var insertionCompletions = [InsertionCompletion]()
-		
-		enum ReceivedMessages: Equatable {
-			case deleteCachedFeed
-			case insert([LocalFeedImage],Date)
-		}
-		
-		private(set) var receivedMessages = [ReceivedMessages]()
-		
-		func deleteCacheFeed(completion: @escaping DeletionCompletion){
-			deletionCompletions.append(completion)
-			receivedMessages.append(.deleteCachedFeed)
-		}
-		
-		func completeDeletion(with error:Error, at index: Int = 0) {
-			deletionCompletions[index](error)
-		}
-		
-		func completeDeletionSuccessfully(at index: Int = 0) {
-			deletionCompletions[index](nil)
-		}
-		
-		func insert(_ feed: [LocalFeedImage],timestamp: Date,completion: @escaping InsertionCompletion) {
-			receivedMessages.append(.insert(feed, timestamp))
-			insertionCompletions.append(completion)
-		}
-		
-		func completeInsertion(with error: Error,at index: Int = 0) {
-			insertionCompletions[index](error)
-		}
-		
-		func completeInsertionSuccessfuly(at index: Int = 0){
-			insertionCompletions[index](nil)
-		}
-	}
-
 }
 
