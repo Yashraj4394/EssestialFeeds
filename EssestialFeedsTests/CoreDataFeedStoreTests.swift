0fc5eba3e18cd2590f36df1f5b883f115da7825b
YashraJ <yashraj.gujar@icloud.com>
2022-04-25 11:43:20 +0530

    Add empty CoreData feed store specs

---
+++ EssestialFeedsTests/CoreDataFeedStoreTests.swift
@@ -0,0 +1,60 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssestialFeedsTests
+//
+//  Created by YashraJ Gujar on 25/04/22.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase,FeedStoreSpecs {
+	
+	func test_retreive_deliversEmptyOnEmptyCache() {
+		
+	}
+	
+	func test_retreive_hasNoSideEffectsOnEmptyCache() {
+		
+	}
+	
+	func test_retreive_deliversFoundValuesOnNonEmptyCache() {
+		
+	}
+	
+	func test_retreive_hasNoSideEffectsOnNonEmtyCache() {
+		
+	}
+	
+	func test_insert_deliversNoErrorOnEmptyCache() {
+		
+	}
+	
+	func test_insert_deliversNoErrorOnNonEmptyCache() {
+		
+	}
+	
+	func test_insert_overridesPreviouslyInsertedCachedValues() {
+		
+	}
+	
+	func test_delete_deliversNoErrorOnEmptyCache() {
+		
+	}
+	
+	func test_delete_hasNoSideEffectsOnEmptyCache() {
+		
+	}
+	
+	func test_delete_deliversNoErrorOnNonEmptyCache() {
+		
+	}
+	
+	func test_delete_emptiesPreviouslyInsertedCache() {
+		
+	}
+	
+	func test_storeSideEffects_runSerially() {
+		
+	}
+	
+}
