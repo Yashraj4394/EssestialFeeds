af2179143f5d9f527e699d436868f72e1c61b5b0
YashraJ <yashraj.gujar@icloud.com>
2022-04-25 12:23:46 +0530

    Add ManagedCache & ManagedFeedImage model representations

--- EssestialFeedsTests/CoreDataFeedStore.swift
+++ EssestialFeedsTests/CoreDataFeedStore.swift
@@ -6,7 +6,7 @@
 //
 
 import EssestialFeeds
-import Foundation
+import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
 	
@@ -24,4 +24,17 @@ public final class CoreDataFeedStore: FeedStore {
 		
 	}
 	
+}
+
+private class ManagedCache : NSManagedObject {
+	@NSManaged var timestamp: Date
+	@NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+	@NSManaged var id: UUID
+	@NSManaged var imageDescription: String?
+	@NSManaged var location: String?
+	@NSManaged var url: URL
+	@NSManaged var cache: ManagedCache
 }
