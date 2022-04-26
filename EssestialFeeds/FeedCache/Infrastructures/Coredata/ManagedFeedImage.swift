3d80aea5091ab9539cc6b8f5e1f074e95880734c
YashraJ <yashraj.gujar@icloud.com>
2022-04-26 23:16:04 +0530

    Separate CoreData managed model classes data from helpers with extensions

--- EssestialFeeds/FeedCache/Infrastructures/Coredata/ManagedFeedImage.swift
+++ EssestialFeeds/FeedCache/Infrastructures/Coredata/ManagedFeedImage.swift
@@ -14,7 +14,9 @@ internal class ManagedFeedImage: NSManagedObject {
 	@NSManaged var location: String?
 	@NSManaged var url: URL
 	@NSManaged var cache: ManagedCache
-	
+}
+
+extension ManagedFeedImage {
 	internal var local: LocalFeedImage {
 		return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
 	}
