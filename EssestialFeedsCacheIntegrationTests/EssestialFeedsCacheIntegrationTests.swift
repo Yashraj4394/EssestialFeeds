a4d7574f751435f1e47acedd17ceda056afadd53
YashraJ <yashraj.gujar@icloud.com>
2022-04-28 12:54:05 +0530

    add 'EssestialFeedsCacheIntegrationTests
    add 'EssestialFeedsCacheIntegrationTests' target to separate the potentially slower cache integration tests from unit/isolated tests

---
+++ EssestialFeedsCacheIntegrationTests/EssestialFeedsCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssestialFeedsCacheIntegrationTests.swift
+//  EssestialFeedsCacheIntegrationTests
+//
+//  Created by YashraJ Gujar on 28/04/22.
+//
+
+import XCTest
+
+///Idea is to integrate all the cache model objects and see how they behave in collaboration with real instances of production types.
+class EssestialFeedsCacheIntegrationTests: XCTestCase {
+
+	
+}
