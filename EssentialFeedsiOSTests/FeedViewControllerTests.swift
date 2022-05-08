d2297263456234262c2fa7a758773320da37ad60
YashraJ <yashraj.gujar@icloud.com>
2022-05-08 11:20:58 +0530

    Decouple testa from specific UI Controls for user
    initiated reloads with test specific DSL method

--- EssentialFeedsiOSTests/FeedViewControllerTests.swift
+++ EssentialFeedsiOSTests/FeedViewControllerTests.swift
@@ -51,17 +51,17 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertEqual(loader.loadingCount, 1)
 	}
 	
-	func test_pullToRefresh_loadsFeed() {
+	func test_userInititatedFeedReload_reloadsFeed() {
 		let (sut , loader) = makeSUT()
 		
 		sut.loadViewIfNeeded()
 		
 		//adding this twice as user can pull to refresh twice
-		sut.refreshControl?.simulatePullToRefresh()
+		sut.simulateUserInitiatedFeedReload()
 		
 		XCTAssertEqual(loader.loadingCount, 2)
 		
-		sut.refreshControl?.simulatePullToRefresh()
+		sut.simulateUserInitiatedFeedReload()
 		
 		XCTAssertEqual(loader.loadingCount, 3)
 	}
@@ -85,18 +85,18 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
 	}
 	
-	func test_pullToRefresh_showsLoadingIndicator() {
+	func test_userInititatedFeedReload_showsLoadingIndicator() {
 		let (sut , _) = makeSUT()
 		
-		sut.refreshControl?.simulatePullToRefresh()
+		sut.simulateUserInitiatedFeedReload()
 		
 		XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
 	}
 	
-	func test_pullToRefresh_hidesLoadingIndicatorOnLoadingCompletion() {
+	func test_userInititatedFeedReload_hidesLoadingIndicatorOnLoadingCompletion() {
 		let (sut , loader) = makeSUT()
 		
-		sut.refreshControl?.simulatePullToRefresh()
+		sut.simulateUserInitiatedFeedReload()
 		
 		loader.completeFeedLoading()
 		
@@ -129,6 +129,12 @@ final class FeedViewControllerTests: XCTestCase {
 		}
 	}
 	
+}
+
+private extension FeedViewController {
+	func simulateUserInitiatedFeedReload() {
+		refreshControl?.simulatePullToRefresh()
+	}
 }
 
 private extension UIRefreshControl {
