53667ca87cd9b77ff47f928fa9191d5c4d71d67d
YashraJ <yashraj.gujar@icloud.com>
2022-05-08 11:01:56 +0530

    show loading indicator on view did load

--- EssentialFeedsiOSTests/FeedViewControllerTests.swift
+++ EssentialFeedsiOSTests/FeedViewControllerTests.swift
@@ -23,6 +23,7 @@ class FeedViewController: UITableViewController {
 		super.viewDidLoad()
 		refreshControl = UIRefreshControl()
 		refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+		refreshControl?.beginRefreshing()
 		load()
 	}
 	
@@ -63,6 +64,14 @@ final class FeedViewControllerTests: XCTestCase {
 		sut.refreshControl?.simulatePullToRefresh()
 		
 		XCTAssertEqual(loader.loadingCount, 3)
+	}
+	
+	func test_viewDidLoad_showsLoadingIndicator() {
+		let (sut , _) = makeSUT()
+		
+		sut.loadViewIfNeeded()
+		
+		XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
 	}
 	
 	//MARK: - HELPERS
